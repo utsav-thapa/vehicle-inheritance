fde9826af8e6ef594443b1d419962d23031de7ec
killioin007-source <killioin.007@gmail.com>
2026-05-12 09:13:08 -0400

    Initial commit

---
+++ src/main/java/inheritance/Car.java
@@ -0,0 +1,7 @@
+public class Car extends Vehicle{
+    public static void main(String[] args) {
+        Car fastCar = new Car();
+        fastCar.setColor("Black");
+        System.out.println(fastCar.getColor());
+    }
+}
---
+++ src/main/java/inheritance/Vehicle.java
@@ -0,0 +1,29 @@
+public class Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+}
